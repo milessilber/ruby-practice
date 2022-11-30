506028e3d8e2ce33cad8c5594ca0de14c08b0ca6
milessilber <milessilber@gmail.com>
2022-11-30 13:03:56 -0500

    Finished isbn assignment other than possibly the last requirement

--- homework/w01/03_isbn/isbn.rb
+++ homework/w01/03_isbn/isbn.rb
@@ -1,3 +1,36 @@
 def isbn_verify?(isbn_string)
-  # Your job is to write the code for this method!
+
+  isbn_array = isbn_string.split("")
+  isbn_array.delete("-")
+  return false if isbn_array.length != 10
+
+  if isbn_array[-1] == "X"
+    isbn_array[-1] = "10"
+  end
+
+
+
+  isbn_array.map!{ |integer| integer.to_i }
+
+  i = 0
+  reverse = 10
+  total_sum = 0
+
+  while i < isbn_array.length
+    total_sum += isbn_array[i] * reverse
+    i += 1
+    if reverse > 0
+      reverse -= 1
+    end
+  end
+
+  if total_sum % 11 == 0
+    return true
+  else
+    return false
+  end
 end
+
+puts isbn_verify?("0-8621-4366-7")
+
+
